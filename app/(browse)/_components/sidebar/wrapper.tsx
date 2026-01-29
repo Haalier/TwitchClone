249e937d6d4bde9f7a5a776ec2cc5e1e188b42d6
Dawid Garncarz <dawid.garncarz@mytracko.pl>
2026-01-29 18:32:39 +0100

    Created sidebar, added zustand store to manage sidebar state

---
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useSidebar } from "@/store/use-sidebar";
+
+interface WrapperProps {
+  children: React.ReactNode;
+}
+
+export const Wrapper = ({ children }: WrapperProps) => {
+  const { collapsed } = useSidebar((state) => state);
+
+  return (
+    <aside
+      className={cn(
+        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-border z-50",
+        collapsed && "w-17.5",
+      )}
+    >
+      {children}
+    </aside>
+  );
+};
