bc8fb013f48973aa18f34874790899e783a82bfa
Dawid Garncarz <dawid.garncarz@mytracko.pl>
2026-02-04 10:21:27 +0100

    Added recommended list to sidebar and skeletons

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -2,18 +2,35 @@
 
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
+import { useEffect, useState } from "react";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
 
 interface WrapperProps {
   children: React.ReactNode;
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
+  const [isClient, setIsClient] = useState(false);
   const { collapsed } = useSidebar((state) => state);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setIsClient(true);
+  }, []);
+
+  if (!isClient)
+    return (
+      <aside className="fixed left-0 flex flex-col w-17.5 lg:w-60 h-full bg-background border-r border-border z-50">
+        <ToggleSkeleton />
+        <RecommendedSkeleton />
+      </aside>
+    );
 
   return (
     <aside
       className={cn(
-        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-border z-50",
+        "fixed left-0 flex flex-col w-17.5 lg:w-60 h-full bg-background border-r border-border z-50",
         collapsed && "w-17.5",
       )}
     >
